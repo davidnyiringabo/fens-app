26a3e61e31db625aa496821e5b42cfd80399f411
grace.fidele <gngrace10@gmail.com>
2023-03-01 15:26:52 +0200

    resolve rebase conflicts

---
+++ src/components/Calendar/Calendar.tsx
@@ -0,0 +1,14 @@
+import Calendar from "react-calendar";
+import './Calendar.css'
+<<<<<<< HEAD
+=======
+import 'react-calendar/dist/Calendar.css'
+>>>>>>> d453fe4 (resolve rebase conflicts)
+
+export default function CalendarModule() {
+    return (
+        <div className="calendar--container">
+            <Calendar />
+        </div>
+    )
+}
