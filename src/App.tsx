1b7dd4942a0adb49c8867f6d1de38081f2578c5d
bayastec <120035666+bayastec@users.noreply.github.com>
2023-03-06 21:29:57 +0200

    scss to css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import reactLogo from './assets/react.svg'
-import {Home} from './pages/home'
+import {Home} from './pages/Home/home'
 import './App.css'
 
 function App() {
