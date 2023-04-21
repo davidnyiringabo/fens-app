d1998ed133cab20e7e8cc5c6cff51e8b10e19c7b
b-sandrine <sanrdushimimana@gmail.com>
2023-04-21 12:16:56 +0200

    Styles for calendar

--- src/components/CalenderComponents.tsx/Body.tsx
+++ src/components/CalenderComponents.tsx/Body.tsx
@@ -13,16 +13,16 @@ export default function CalendarBody({date}: {date: Date}) {
         daysInMonth.push(new Date(year,month, i))
     }
     
-    const days = ['Sun','Mon','Tue', 'Wed','Thu', 'Fri','Sat'];
+    const days = ['Sunday','Monday','Tuesday', 'Wednesday','Thursday', 'Friday','Saturday'];
     return (
         <div className="calendar--body">
             {days.map((day) => (
-                <div className="calendar--day" key={day}>
+                <div className="calendar--day--name" key={day}>
                     {day}
                 </div>
             ))}
             {daysInMonth.map((day) => (
-                <div className="calendar--day" key={day.toString()} >
+                <div className="calendar--day--date" key={day.toString()} >
                     {day.getDate()}
                 </div>
             ))}
