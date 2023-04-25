d7573d72ba287ae4141c9f39c41c797877218545
b-sandrine <sanrdushimimana@gmail.com>
2023-04-25 10:34:03 +0200

    Solved error in schedule

--- src/pages/Schedule/Schedule.tsx
+++ src/pages/Schedule/Schedule.tsx
@@ -1,9 +1,5 @@
 import ScheduleList from "../../components/SchedulesList/ScheduleList";
-<<<<<<< HEAD
 import Calendar from "../../components/Calendar/Calendar"
-=======
-import Calendar from "../../components/CalendarModule/Calendar"
->>>>>>> 0f7f665 (new page added --help page)
 import './Schedule.css'
 
 export default function Schedules () {
